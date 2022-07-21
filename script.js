824c7252d6317a40c8b131f27f33fc06ecec06f6
Codedwells <wells.mail.13@gmail.com>
2022-07-21 22:40:10 +0300

    random number function added

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ bidDownBtn = document.getElementById("bidDown");
 bidAmountCount = document.getElementById("bidAmount");
 remCredits = document.getElementById("creditsAvailable");
 dealerMessage = document.getElementById("msgBox");
+var randomNum = null;
+
+//DOM manipulation coming right up :)
+//bid change
+function getRnmNumber() {
+	randomNum = Math.floor(Math.random() * 13) + 1;
+}
+bidUpBtn.addEventListener("click", function () {
+	bidAmountCount.textContent++;
+});
+bidDownBtn.addEventListener("click", function () {
+	bidAmountCount.textContent--;
+	if (bidAmountCount.textContent < 0) {
+		bidAmountCount.textContent = 0;
+	}
+});
+//New card
+newCardBtn.addEventListener("click", function () {
+	getRnmNumber();
+});
